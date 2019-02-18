26740e6a8ea6d7c4f0f388cfbdea96a8da013185
Justin Sitter <justin@jaytria.com>
2019-02-17 20:50:35 -0800

    Add stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import Nav from './components/Nav';
+import './css/style.css';
 
 class App extends Component {
   render(){
