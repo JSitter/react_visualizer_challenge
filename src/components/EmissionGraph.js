3126b3eab33286a19ebd4429c0a2aef3fd4d628f
Justin Sitter <justin@jaytria.com>
2019-02-18 11:01:03 -0800

    Add EmissionsGraph Component

---
+++ src/components/EmissionGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EmissionGraph extends Component {
+  render(){
+    return (
+      <h2>Emissions</h2>
+    )
+  }
+}
+
+export default EmissionGraph;   
