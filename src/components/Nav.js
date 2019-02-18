0e5a487af58a34e3a629a14979ca7e4fd084f66c
Justin Sitter <justin@jaytria.com>
2019-02-18 11:02:40 -0800

    Add materialize navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 class Nav extends Component {
   render(){
     return (
-      <p>My navigation</p>
+        <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo right">Logo</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><Link to="/emissions">Emissions</Link></li>
+        <li><Link to="/population">Population</Link></li>
+        <li><Link to="/temperatures">Temperatures</Link></li>
+      </ul>
+    </div>
+  </nav>
     )
   }
 }
