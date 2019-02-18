ffd3eb641be081c5a45a717d2fe8e421dbf74990
Justin Sitter <justin@jaytria.com>
2019-02-18 11:01:38 -0800

    Add basic PopulationGraph Component

---
+++ src/components/PopulationGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PopulationGraph extends Component {
+  render(){
+    return (
+      <h2>Population over Time</h2>
+    )
+  }
+}
+
+export default PopulationGraph;   
