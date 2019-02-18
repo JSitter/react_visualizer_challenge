8c30ab5b84a9eabc343cff6e9212dbbfd9f95f08
Justin Sitter <justin@jaytria.com>
2019-02-18 12:50:40 -0800

    switch components on route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import EmissionGraph from './components/EmissionGraph';
+import Introduction from './components/Introduction';
 import Nav from './components/Nav';
+import PopulationGraph from './components/PopulationGraph';
+import TemperatureGraph from './components/TemperatureGraph';
+
 import './css/style.css';
 
 class App extends Component {
@@ -14,8 +20,6 @@ class App extends Component {
 
     };
 
-
-
   updateUrl(url){
     this.setState({url});
   };
@@ -25,10 +29,14 @@ class App extends Component {
       <BrowserRouter>
         <section>
           <Nav updateUrl={this.updateUrl}/>
-          Url:
-          {this.state.url}
-          <p>Hello Google!</p>
+          
+          
+          <Route exact path="/" component={Introduction} />
+          <Route path="/emissions" component={EmissionGraph} />
+          <Route path="/population" component={PopulationGraph} />
+          <Route path="/temperatures" component={TemperatureGraph} />
 
+          <section id="debug">Url: {this.state.url}</section>
         </section>
       </BrowserRouter>
     );
