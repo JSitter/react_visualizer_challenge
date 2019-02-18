fdd6060596564dc69c08a38c57e90a0cabf938af
Justin Sitter <justin@jaytria.com>
2019-02-18 11:02:13 -0800

    Add basic TemperatureGraph component

---
+++ src/components/TemperatureGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TemperatureGraph extends Component {
+  render(){
+    return (
+      <h2>Temperature Over Time</h2>
+    )
+  }
+}
+
+export default TemperatureGraph;   
