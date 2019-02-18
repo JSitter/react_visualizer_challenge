1171c35eb177da68a415288f465fc9158ea20d4e
Justin Sitter <justin@jaytria.com>
2019-02-18 12:22:53 -0800

    call updateUrl on navbar click

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 class Nav extends Component {
+  constructor(props){
+    super(props)
+  }
   render(){
     return (
         <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo right">Logo</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><Link to="/emissions">Emissions</Link></li>
-        <li><Link to="/population">Population</Link></li>
-        <li><Link to="/temperatures">Temperatures</Link></li>
+        <li onClick={()=>this.props.updateUrl("emissions")}><Link to="/emissions">Emissions</Link></li>
+        <li onClick={()=>this.props.updateUrl("population")}><Link to="/population">Population</Link></li>
+        <li onClick={()=>this.props.updateUrl("temperatures")}><Link to="/temperatures">Temperatures</Link></li>
       </ul>
     </div>
   </nav>
