951a36a259e7dc73fda47cfbf5d7fcf43a4653a6
Justin Sitter <justin@jaytria.com>
2019-02-18 12:33:45 -0800

    add Introduction component

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Introduction extends Component {
+  render(){
+    return (
+      <section>
+        <h2>Indroduction</h2>
+        <p>These datasets are meant to be a visualized look at certain API endpoints.</p>
+      </section>
+    )
+  }
+}
+
+export default Introduction;  
