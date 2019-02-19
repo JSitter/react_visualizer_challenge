dcb32507af6ee0cb72e4470affeff03504257aa9
Justin Sitter <justin@jaytria.com>
2019-02-18 16:35:19 -0800

    add routes

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+//Front end routes that may be hit by the back end.  
+app.get('/emissions', (req, res)=>{
+  res.redirect("/");
+})
+app.get('/population', (req, res)=>{
+  res.redirect("/");
+})
+app.get('/temperatures', (req, res)=>{
+  res.redirect("/");
+})
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
