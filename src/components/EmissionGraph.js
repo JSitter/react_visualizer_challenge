a64268d93afe675e6ae87b6cae4f0c873d95054b
Justin Sitter <justin@jaytria.com>
2019-02-19 23:07:57 -0800

    Add props to LineChart

--- src/components/EmissionGraph.js
+++ src/components/EmissionGraph.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import LineChart from './LineChart';
 
 class EmissionGraph extends Component {
+  constructor(props){
+    super(props)
+
+  }
+
   render(){
     return (
-      <h2>Emissions</h2>
+      <section id="main">
+        <h2>Emissions</h2>
+        <LineChart emissionData={this.props.emissionData} xSize={800} ySize={800} xAxisText={'Total Carbon in Birds per Second'}/>
+      </section>
     )
   }
 }
