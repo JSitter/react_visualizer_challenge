6a9b9d7480e6ed8a39dcd238741c2f2c3fb1f1a1
Justin Sitter <justin@jaytria.com>
2019-02-19 23:00:47 -0800

    add main id

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Introduction extends Component {
   render(){
     return (
-      <section>
+      <section id="main">
         <h2>Indroduction</h2>
         <p>These datasets are meant to be a visualized look at certain API endpoints.</p>
       </section>
