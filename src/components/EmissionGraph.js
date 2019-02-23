311db21c1b24a41fb1e06399a8e0fa8404a8d246
Justin Sitter <justin@jaytria.com>
2019-02-22 18:03:00 -0800

    Add data cleaning and limiting logic

--- src/components/EmissionGraph.js
+++ src/components/EmissionGraph.js
@@ -3,15 +3,53 @@ import LineChart from './LineChart';
 
 class EmissionGraph extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      'filteredData': []
+    }
+    this.firstElement = {}
+ }
 
+  componentDidUpdate(){
+
+    // Only clean data if new
+    if ('Year' in this.props.emissionData[0]){
+
+      if ('Year' in this.firstElement ){
+        if (this.firstElement.Year != this.props.emissionData[0].Year){
+
+          let cleaned = this.cleanData(this.props.emissionData)
+          this.setState({'filteredData': cleaned})
+          this.firstElement = this.props.emissionData[0]
+        }
+      } else {
+        let cleaned = this.cleanData(this.props.emissionData)
+        this.setState({filteredData: cleaned})
+        this.firstElement = this.props.emissionData[0]
+      }
+    }
+    
+  }
+
+  cleanData(){
+    let data = this.props.emissionData;
+
+    // Remove NaN's from Dataset
+    let filteredData = []
+    if (data.length>0){
+       data.forEach((d)=>{
+          if (!isNaN(d.Year) || !isNaN(d.Total))
+             filteredData.push({'X':d.Year, 'Y':d.Total})
+       })
+    }
+    return filteredData
   }
 
   render(){
     return (
       <section id="main">
         <h2>Emissions</h2>
-        <LineChart emissionData={this.props.emissionData} xSize={800} ySize={800} xAxisText={'Total Carbon in Something per Hour'}/>
+        <LineChart data={this.state.filteredData} xSize={1200} ySize={400} xAxisText={'Total Carbon in Million Metric Tons'}/>
       </section>
     )
   }
