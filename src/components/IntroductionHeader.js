e426a0b81409877020925b4257f3c6582611106d
Justin Sitter <justin@jaytria.com>
2019-02-22 21:45:52 -0800

    add header for Landing page

---
+++ src/components/IntroductionHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class IntroductionHeader extends Component {
+  render(){
+    return (
+      <header id='introduction-header'>
+        <h1>Dataset Challenge</h1>
+
+      </header>
+    )
+  }
+};
+
+export default IntroductionHeader;
