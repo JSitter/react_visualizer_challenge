73b09c13b3c167601a2d1a45e9d3ba1a435b3945
Justin Sitter <justin@jaytria.com>
2019-02-22 21:45:27 -0800

    Add header for emission page

---
+++ src/components/EmissionHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EmissionHeader extends Component {
+  render(){
+    return (
+      <header id='emission-header'>
+        <h1>Global Emissions</h1>
+
+      </header>
+    );
+  };
+};
+
+export default EmissionHeader;
