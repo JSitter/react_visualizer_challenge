74af657e1045db37a04e958bf7b9b034685ae3a3
Justin Sitter <justin@jaytria.com>
2019-02-22 21:46:31 -0800

    add header for temperature visualization

---
+++ src/components/TemperatureHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TemperatureHeader extends Component {
+  render(){
+    return (
+      <header id='temperature-header'>
+        <h1>Global Temperatures</h1>
+
+      </header>
+    );
+  };
+};
+
+export default TemperatureHeader;
