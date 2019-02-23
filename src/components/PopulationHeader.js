f2575a24c8a19a48604f09581da869d4471eba8d
Justin Sitter <justin@jaytria.com>
2019-02-22 21:46:12 -0800

    add header for population visualization

---
+++ src/components/PopulationHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class PopulationHeader extends Component {
+  render(){
+    return (
+      <header id='population-header'>
+        <h1>Population</h1>
+
+      </header>
+    );
+  };
+};
+
+export default PopulationHeader;
