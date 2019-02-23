17e46f663a7b85659615504cd22509af3c6b766d
Justin Sitter <justin@jaytria.com>
2019-02-22 19:00:11 -0800

    formatting

--- src/components/EmissionGraph.js
+++ src/components/EmissionGraph.js
@@ -6,30 +6,28 @@ class EmissionGraph extends Component {
     super(props);
     this.state = {
       'filteredData': []
-    }
-    this.firstElement = {}
- }
+    };
+    this.firstElement = {};
+ };
 
   componentDidUpdate(){
 
     // Only clean data if new
     if ('Year' in this.props.emissionData[0]){
-
       if ('Year' in this.firstElement ){
         if (this.firstElement.Year != this.props.emissionData[0].Year){
-
           let cleaned = this.cleanData(this.props.emissionData)
           this.setState({'filteredData': cleaned})
           this.firstElement = this.props.emissionData[0]
-        }
+        };
       } else {
         let cleaned = this.cleanData(this.props.emissionData)
         this.setState({filteredData: cleaned})
         this.firstElement = this.props.emissionData[0]
-      }
-    }
+      };
+    };
     
-  }
+  };
 
   cleanData(){
     let data = this.props.emissionData;
@@ -39,11 +37,11 @@ class EmissionGraph extends Component {
     if (data.length>0){
        data.forEach((d)=>{
           if (!isNaN(d.Year) || !isNaN(d.Total))
-             filteredData.push({'X':d.Year, 'Y':d.Total})
-       })
-    }
-    return filteredData
-  }
+             filteredData.push({'X':d.Year, 'Y':d.Total});
+       });
+    };
+    return filteredData;
+  };
 
   render(){
     return (
@@ -51,8 +49,8 @@ class EmissionGraph extends Component {
         <h2>Emissions</h2>
         <LineChart data={this.state.filteredData} xSize={1200} ySize={400} xAxisText={'Total Carbon in Million Metric Tons'}/>
       </section>
-    )
-  }
-}
+    );
+  };
+};
 
 export default EmissionGraph;   
